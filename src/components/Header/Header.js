862d6227f6c7100a587689f03d00a58c86d6f58c
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-26 22:02:06 +0600

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h1 className="text-3xl font-bold underline">
+            <h1 className="text-4xl font-bold underline">
                 Hello world!
             </h1>
         </div>
