920805c80dddf286d5c581e3b5f7a28aa310fcd7
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-26 23:12:50 +0600

    updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Logo from '../../images/Logo.svg';
 import './Header.css'
 const Header = () => {
     return (
-        <div className='navbar flex justify-between px-24'>
+        <div className='navbar flex justify-between px-5 lg:px-24'>
             <div>
                 <a href="/home">
                     <img src={Logo} alt="EmaJhon" />
@@ -15,6 +15,11 @@ const Header = () => {
                 <a href="order-review">Order Review</a>
                 <a href="/inventory">Manage Inventory</a>
                 <a href="/login">Login</a>
+            </div>
+            <div className='menubar text-white'>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
             </div>
         </div>
     );
