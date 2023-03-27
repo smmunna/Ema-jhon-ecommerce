46ff44d09bc5e4d6755108d6c0db05a0f7837599
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-27 17:05:07 +0600

    Updated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import './Products.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping } from '@fortawesome/free-solid-svg-icons'
 
 const Products = (props) => {
     const{name,img,price,seller,ratings}=props.product;
+    const handleAddtoCart = props.handleAddtoCart;
     return (
         <div>
             <div className="card w-full h-full bg-base-100 shadow-xl">
@@ -16,7 +19,12 @@ const Products = (props) => {
                         Ratings: {ratings} stars
                     </p>
                     <div className="card-actions">
-                        <button className="btn btn-primary">Buy Now</button>
+                        <button className="btn btn-primary" onClick={()=>handleAddtoCart(props.product)}>
+                        Buy Now
+                       <span className='ml-3'>
+                       <FontAwesomeIcon icon={faBagShopping} size="2x" />
+                       </span>
+                        </button>
                     </div>
                 </div>
             </div>
--- src/components/Shop/ShopContainer.js
+++ src/components/Shop/ShopContainer.js
@@ -4,11 +4,19 @@ import './ShopContainer.css';
 
 const ShopContainer = () => {
     const[products,setProducts] = useState([]);
+    const[cart,setCart] = useState([]);
     useEffect(()=>{
         fetch('https://raw.githubusercontent.com/smmunna/json-data/main/products.json')
         .then(res=>res.json())
         .then(data=>setProducts(data));
     },[])
+
+    // function for add to Cart Button, from Product.js
+    const handleAddtoCart = (product)=>{
+        const newCart = [...cart,product];
+        setCart(newCart);
+    }
+
     return (
         <div className='shop-container lg:p-5 lg:mt-5'>
              <div className="shop-card grid grid-cols-1 md:grid-cols-3 gap-3">
@@ -16,11 +24,13 @@ const ShopContainer = () => {
                     products.map(product=><Products
                         key={product.id}
                         product={product}
+                        handleAddtoCart={handleAddtoCart}
                     />)
                 }
              </div>
-             <div className="order-summary">
-                <h3>Order Summary</h3>
+             <div className="order-summary lg:px-3 text-center">
+                <h3 className='text-2xl font-bold'>Order Summary</h3>
+                <h3>Selected Items:{cart.length}</h3>
              </div>
         </div>
     );
