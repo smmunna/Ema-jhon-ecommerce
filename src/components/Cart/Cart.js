194b8da028aa657616f35ce664d3551707b1ebd3
Minhazul Abedin Munna <minhazulabedinmunna@gmail.com>
2023-03-27 22:25:09 +0600

    Update Cart.js

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -18,7 +18,7 @@ const Cart = (props) => {
            
             <h3>Price: {total}$</h3>
             <h3>Total Shipping: ${totalShipping} </h3>
-            <h3>Tax: {tax}%  </h3>
+            <h3>Tax: ${tax}  </h3>
             <h3>Grand Total: ${grandTotal}</h3>
         </div>
     );
